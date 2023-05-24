6ea6f5f5b3c2c2d9dbe8cc7bc801663da2d19f09
Noam Amrani <noama@sela.co.il>
2023-05-24 19:17:41 +0300

    init commit

---
+++ ConsoleApp3/FileName.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public static class BinaryTree
+{
+    public static bool IsSortedByLevels(TreeNode root)
+    {
+        if (root == null)
+            return true;
+
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        int prevVal = int.MinValue;
+
+        while (queue.Count > 0)
+        {
+            TreeNode node = queue.Dequeue();
+            if (node.val < prevVal)
+                return false;
+
+            prevVal = node.val;
+            if (node.left != null)
+                queue.Enqueue(node.left);
+            if (node.right != null)
+                queue.Enqueue(node.right);
+        }
+        return true;
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Create binary tree: 1, 3, 4, 6, 6, 7, 7, 8, 9
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(3);
+        root1.right = new TreeNode(4);
+        root1.left.left = new TreeNode(6);
+        root1.left.right = new TreeNode(6);
+        root1.right.left = new TreeNode(7);
+        root1.right.right = new TreeNode(7);
+        root1.left.left.left = new TreeNode(8);
+        root1.left.left.right = new TreeNode(9);
+        Console.WriteLine("Expected: True, Actual: " + BinaryTree.IsSortedByLevels(root1));
+
+        // Create binary tree: 1, 3, 2
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(3);
+        root2.right = new TreeNode(2);
+        Console.WriteLine("Expected: False, Actual: " + BinaryTree.IsSortedByLevels(root2));
+
+        // Create binary tree: 1
+        TreeNode root3 = new TreeNode(1);
+        Console.WriteLine("Expected: True, Actual: " + BinaryTree.IsSortedByLevels(root3));
+
+        // Empty binary tree
+        Console.WriteLine("Expected: True, Actual: " + BinaryTree.IsSortedByLevels(null));
+    }
+}
